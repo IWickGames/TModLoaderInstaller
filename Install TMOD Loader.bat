53f88af63ac325c0e0e5e3a299b5f388b67c8f26
IWickGames <smarttecgaming@gmail.com>
2019-08-04 21:02:50 -0500

    First Upload

---
+++ Install TMOD Loader.bat
@@ -0,0 +1,55 @@
+@echo off
+if not exist update.dat goto update
+mode 70,25
+title TModLoader Install
+:start
+echo.
+echo        Please enter your Terraria install directory
+echo        EX: D:\SteamLibrary\steamapps\common\Terraria
+echo.
+set /p installdir="InstallDir>"
+set version=v0.11.3
+set download=https://github.com/tModLoader/tModLoader/releases/download/v0.11.3/tModLoader.Windows.v0.11.3.zip
+echo Starting Powershell
+echo Running wget ...
+powershell wget "%download%" -OutFile "tmodloader.zip"
+if not exist tmodloader.zip goto downloaderror
+echo Downloaded TModLoader%version%
+echo Checking to back up your old copy...
+if not exist "%installdir%\Terraria-OLD.zip" goto backup
+:endbackup
+echo Extracting Modded Copy...
+xcopy "tmodloader.zip" "%installdir%">nul
+7za x -y -o"%installdir%" "%installdir%\tmodloader.zip">nul
+echo Extracted TModLoader%version%
+echo Done!
+echo Cleaning up install...
+del /f /q "tmodloader.zip"
+del /f /q "%installdir%\tmodloader.zip"
+del /f /q "update.dat"
+echo ------------------------------------------------------------------
+echo                     Install compleated!
+echo Check to make sure that everything is working by starting the game
+echo.
+echo.
+echo.
+echo                    Press any key to exit
+pause >nul
+exit
+
+:backup
+echo Backing up your old copy...
+7za a "Terraria-OLD.zip" "D:\SteamLibrary\steamapps\common\Terraria\">nul
+move "Terraria-OLD.zip" "D:\SteamLibrary\steamapps\common\Terraria">nul
+echo Done!
+goto endbackup
+
+:downloaderror
+echo Error: Something went wrong wile trying to download the TModLoader.zip file...
+echo Press any key to retry!
+pause >nul
+goto start
+
+:update
+start "" "update.dat"
+exit
---
+++ update.bat
@@ -0,0 +1,3 @@
+@echo off
+echo Starting Update...
+powershell wget "%download%" -OutFile "tmodloader.zip"
