db82b704605d557e7ebfec2cca8f4c26a1205f8e
IWickGames <smarttecgaming@gmail.com>
2019-10-12 15:20:50 -0500

    Update Install TMOD Loader.bat

--- Install TMOD Loader.bat
+++ Install TMOD Loader.bat
@@ -9,7 +9,7 @@ echo        EX: E:\SteamLibrary\steamapps\common\Terraria
 echo.
 set /p installdir="InstallDir>"
 set version=v0.11.4
-set download=https://github.com/tModLoader/tModLoader/releases/download/v0.11.4/tModLoader.Windows.v0.11.4.zip
+set download=https://github.com/tModLoader/tModLoader/releases/download/v0.11.5/tModLoader.Windows.v0.11.5.zip
 echo Starting Powershell
 echo Running wget ...
 powershell wget "%download%" -OutFile "tmodloader.zip">nul
